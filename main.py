b2800cd9c9235adf246ae0541c89c20ede51dede
Lucas Magueta <127806458+WMagueta@users.noreply.github.com>
2023-08-21 16:17:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from turtle import Turtle, Screen
 import random
+import tkinter as tk
 
-is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 screen.title("Turtle Race")
-user_bet = screen.textinput(title="Make your bet", prompt="Wich turtle will win the race? Enter a color.")
+screen.bgcolor("black")
+
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions=[-70, -40, -10, 20, 50, 80]
 all_turtles=[]
+
+user_bet = screen.textinput(title="Make your bet", prompt="Wich turtle will win the race? Enter a color.")
 
 for turtle_index in range(0, 6):
     tim = Turtle(shape="turtle")
@@ -17,6 +20,7 @@ for turtle_index in range(0, 6):
     tim.goto(-230, y=y_positions[turtle_index])
     all_turtles.append(tim)
 
+is_race_on = False
 if user_bet:
     is_race_on = True
 
@@ -28,9 +32,15 @@ while is_race_on:
             is_race_on = False
             winner = turtle.color()[1]
 
+tim = Turtle()
+tim.penup()
+tim.color("white")
+tim.hideturtle()
+tim.goto(-200, 100)
+
 if winner == user_bet:
-    print(f"You won the bet! {winner} won")
+    tim.write(f"You won the bet! {winner} won.", font=("Arial",18,"normal"))
 else:
-    print(f"You lost the bet! {winner} won.")
+    tim.write(f"You lost the bet! {winner} won.", font=("Arial",18,"normal"))
 
 screen.exitonclick()
